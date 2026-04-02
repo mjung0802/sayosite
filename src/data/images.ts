2a8f2defd1731fd0e86e7c8fc37a6418a7ea1895
mjung0802 <daniel0802@gmail.com>
2026-04-02 15:22:36 -0700

    happy to help wip images

--- src/data/images.ts
+++ src/data/images.ts
@@ -2,6 +2,6 @@ export const SECTION_IMAGES: Record<string, string[]> = {
   '/about': ['about1.png', 'about2.jpg', 'about3.jpg', 'about4.jpg'],
   '/projects/tone-chat': ['project1-1.png', 'project1-2.png', 'project1-3.png', 'project1-4.png'],
   '/projects/ngu-guide': ['project2-1.png', 'project2-2.png', 'project2-3.png', 'project2-4.png'],
-  '/projects/happytohelp': ['project3-1.jpg', 'project3-2.jpg', 'project3-3.jpg', 'project3-4.jpg'],
+  '/projects/happytohelp': ['project3-1.png', 'project3-2.png', 'project3-3.png', 'project3-4.png'],
   '/hobbies': ['hobbies1.png', 'hobbies2.jpg', 'hobbies3.jpg', 'hobbies4.jpg'],
 }
